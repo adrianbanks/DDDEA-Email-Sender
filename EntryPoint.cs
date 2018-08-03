d10758aeac5251001910325d2395b4758a4be7ee
Adrian Banks <github@adrianbanks.co.uk>
2018-08-03 16:23:08 +0100

    Add better help output

--- EntryPoint.cs
+++ EntryPoint.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 
 namespace DDDEastAnglia.EmailSender
 {
@@ -13,10 +14,13 @@ namespace DDDEastAnglia.EmailSender
                 if (emailType == null)
                 {
                     Console.WriteLine("Could not figure out email type. No emails have been sent.");
+                    var values = (EmailTypes[]) Enum.GetValues(typeof(EmailTypes));
+                    var validOptions = string.Join(" ", values.Select(v => v.ToString()));
+                    Console.WriteLine("Valid options are: {0}", validOptions);
                     return;
                 }
 
-                Console.WriteLine($"Sending {emailType} emails...");
+                Console.WriteLine($"Sending {emailType.ToString().ToLower()} emails...");
                 var program = new Program(new Configuration());
                 program.Run(emailType.Value);
 
